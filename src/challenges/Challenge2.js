8aa2cd4cd1bc618a4edf02b345b54cc229924944
Andrew Gross <92811948+andrew-gross@users.noreply.github.com>
2021-11-17 09:11:31 -0700

    a few fixes to challenge 2

--- src/challenges/Challenge2.js
+++ src/challenges/Challenge2.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Button from "../Button";
 
 /*
    Find the bug in this component
@@ -12,37 +11,38 @@ export default function Challenge2() {
   const [todos, setTodos] = React.useState([]);
 
   const onAddTodo = () => {
-    setTodos([...todos, todo]);
+    setTodos([todo]);
+    setTodo("");
   };
 
   return (
     <>
-      <div className="mb-10">
-        <h2 className="text-2xl mb-2">Challenge 2</h2>
+      <div className="challenge-header">
+        <h2>Challenge 2</h2>
         <p>
           When the user enters a todo into the text field and then clicks the
           "add button" a todo should be added to the list of todos.
         </p>
       </div>
-      <div>
+      <div className="challenge-content">
         <input
           type="text"
-          className="px-8 py-2 rounded shadow border border-gray-400"
+          className="text-input"
           placeholder="Enter a todo"
           value={todo}
           onChange={(e) => setTodo(e.target.value)}
         />
-        <Button onClick={onAddTodo}>Add Todo</Button>
-        <div className="my-4">
-          <h3 className="text-xl mb-1">Todos</h3>
-          <div>
-            {todos.length > 0 &&
-              todos.map((t) => (
-                <div className="py-1 border-b border-gray-300 odd:bg-gray-100 last:border-0">
-                  {t}
-                </div>
-              ))}
-          </div>
+        <button className="button" onClick={onAddTodo}>
+          Add Todo
+        </button>
+        <div className="todos">
+          <h3 className="todo-header">Todos</h3>
+          {todos.length > 0 &&
+            todos.map((t, i) => (
+              <div key={i} className="todo">
+                {t}
+              </div>
+            ))}
         </div>
       </div>
     </>
